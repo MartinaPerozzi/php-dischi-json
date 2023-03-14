636503c29a635b345fdf51a211b6f57668539406
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-14 16:02:04 +0100

    + style

--- index.php
+++ index.php
@@ -28,20 +28,33 @@
 <body class="bg-[#1c2d3b]">
     <div id="app">
         <div class="w-full">
-            <header class="bg-[#101f30] w-full p-2">
-                <div class="flex align-center p-3">
+            <header class="bg-[#101f30] w-full p-2 mb-5 px-5">
+                <div class="flex items-center p-3">
                     <img src="./img/spotify.png" alt="logo spotify" class="w-16">
                 </div>
             </header>
 
             <main>
+                <div class="flex flex-wrap w-2/3 mx-auto gap-4">
+                    <div class="bg-[#101f30] text-white m-3 w-52" v-for="disk in diskList">
+                        <div class="flex flex-col items-center justify-center text-center">
+                            <img :src="disk.poster" alt="Disk poster" class="w-36 pt-3">
+                            <div class="pt-3">
+                                <h3 class="text-sm px-3 h-10">{{disk.title}}</h3>
+                                <span class="text-sm font-extralight">{{disk.author }}</span>
+                                <h4 class="text-sm pb-4 pt-2">{{disk.year}}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-
-            </main>
-
-
-            <h1 class="text-white">PROVA {{diskList}} </h1>
         </div>
+
+        </main>
+
+        <!-- 
+        <h1 class="text-white">PROVA {{diskList}} </h1> -->
+    </div>
 
     </div>
 
